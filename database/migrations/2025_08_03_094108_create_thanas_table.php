3043a7ba211e4d6ccc098d91500ed406629500cc
Emon943 <motiur943@gmail.com>
2025-08-11 17:17:25 +0600

    Districts and  Thana table field name change

--- database/migrations/2025_08_03_094108_create_thanas_table.php
+++ database/migrations/2025_08_03_094108_create_thanas_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('thanas', function (Blueprint $table) {
             $table->id();
             $table->foreignId('district_id')->constrained()->onDelete('cascade');
-            $table->string('name');
+            $table->string('thana_name');
             $table->timestamps();
         });
     }
